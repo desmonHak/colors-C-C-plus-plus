64678aba2a1202b240e6deddfc8258baa773a9bc
Desmon <61366533+desmonHak@users.noreply.github.com>
2022-01-23 20:19:40 +0100

    Update colors.h

--- colors.h
+++ colors.h
@@ -1,104 +1,45 @@
 #if !defined(__COLORS__)
-#define __COLORS__
+#define __COLORS__ "Desmon"
 
 #include <stdio.h>
 
-typedef struct _color
-{
-    const char *BLACK;  //"\033[30m"
-    const char *RED;    //"\033[31m"
-    const char *GREEN;  //"\033[32m"
-    const char *YELLOW; //"\033[33m"
-    const char *BLUE;   //"\033[34m"
-    const char *MAGENTA;//"\033[35m"
-    const char *CYAN;   //"\033[36m"
-    const char *WHITE;  //"\033[37m"
-    const char *RESET;  //"\033[38m"
+#define BACKGROUND_BLACK        "\e[0;40m"
+#define BACKGROUND_RED          "\e[0;41m"
+#define BACKGROUND_GREEN        "\e[0;42m"
+#define BACKGROUND_YELLOW       "\e[0;43m"
+#define BACKGROUND_BLUE         "\e[0;44m"
+#define BACKGROUND_PURPLE       "\e[0;45m"
+#define BACKGROUND_CYAN         "\e[0;46m"
+#define BACKGROUND_WHITE        "\e[0;47m"
+#define BACKGROUND_RESET        "\e[0m"
 
-    const char *LIGHTBLACK_EX;  //"\033[90m"
-    const char *LIGHTRED_EX;    //"\033[91m"
-    const char *LIGHTGREEN_EX;  //"\033[92m"
-    const char *LIGHTYELLOW_EX; //"\033[93m"
-    const char *LIGHTBLUE_EX;   //"\033[94m"
-    const char *LIGHTMAGENTA_EX;//"\033[95m"
-    const char *LIGHTCYAN_EX;   //"\033[96m"
-    const char *LIGHTWHITE_EX;  //"\033[97m"
+#define LETTER_BLACK            "\033[30m"
+#define LETTER_RED              "\033[31m"
+#define LETTER_GREEN            "\033[32m"
+#define LETTER_YELLOW           "\033[33m"
+#define LETTER_BLUE             "\033[34m"
+#define LETTER_MAGENTA          "\033[35m"
+#define LETTER_CYAN             "\033[36m"
+#define LETTER_WHITE            "\033[37m"
+#define LETTER_RESET            "\033[37m\033[38m"
+#define LETTER_LIGHTBLACK_EX    "\033[90m"
+#define LETTER_LIGHTRED_EX      "\033[91m"
+#define LETTER_LIGHTGREEN_EX    "\033[92m"
+#define LETTER_LIGHTYELLOW_EX   "\033[93m"
+#define LETTER_LIGHTBLUE_EX     "\033[94m"
+#define LETTER_LIGHTMAGENTA_EX  "\033[95m"
+#define LETTER_LIGHTCYAN_EX     "\033[96m"
+#define LETTER_LIGHTWHITE_EX    "\033[97m"
 
-    void (*UP)(const char *, int);
-    void (*DOWN)(const char *, int);
-    void (*FORWARD)(const char *, int);
-    void (*BACK)(const char *, int);
-    void (*POS)(const char *, int, int);
-    void (*SET_TITLE)(const char *);
-    void (*CLEAR)(void);
 
-} _color;
-
-void _UP(const char *data, int number)
-{
-    printf("\033[%dA%s", number, data);
-}
-void _DOWN(const char *data, int number)
-{
-    printf("\033[%dB%s", number, data);
-}
-void _FORWARD(const char *data, int number)
-{
-    printf("\033[%dC%s", number, data);
-}
-void _BACK(const char *data, int number)
-{
-    printf("\033[%dD%s", number, data);
-}
-void _POS(const char *data, int number1, int number2)
-{
-    printf("\033[%d;%dH%s", number1, number2, data);
-}
-void _SET_TITLE(const char *title)
-{
-    printf("\033]2;%s\007", title);
-}
-void _CLEAR(void)
-{
-    printf("\033[3J\033[H\033[2J");
-}
-void POINTGREEN(const char *data, _color color)
-{
-    printf("%s[%s*%s]%s %s", color.LIGHTGREEN_EX, color.BLUE, color.LIGHTGREEN_EX, color.LIGHTWHITE_EX, data, color.RESET);
-}
-void POINTRED(const char *data, _color color)
-{
-    printf("%s[%s*%s]%s %s", color.LIGHTYELLOW_EX, color.RED, color.LIGHTYELLOW_EX, color.LIGHTMAGENTA_EX, data, color.RESET);
-}
-
-const _color color = {
-    "\033[30m",
-    "\033[31m",
-    "\033[32m",
-    "\033[33m",
-    "\033[34m",
-    "\033[35m",
-    "\033[36m",
-    "\033[37m",
-    "\033[37m\033[38m",
-
-    "\033[90m",
-    "\033[91m",
-    "\033[92m",
-    "\033[93m",
-    "\033[94m",
-    "\033[95m",
-    "\033[96m",
-    "\033[97m",
-
-    _UP,
-    _DOWN,
-    _FORWARD,
-    _BACK,
-    _POS,
-    _SET_TITLE,
-    _CLEAR,
-
-};
+#define UP(data, number)                printf("\033[%dA%s", number, data);
+#define DOWN(data, number)              printf("\033[%dB%s", number, data);
+#define FORWARD(data, number)           printf("\033[%dC%s", number, data);
+#define BACK(data, number)              printf("\033[%dD%s", number, data);
+#define POS(data, number1, number2)     printf("\033[%d;%dH%s", number1, number2, data);
+#define SET_TITLE(title)                printf("\033]2;%s\007", title);
+#define CLEAR()                         printf("\033[3J\033[H\033[2J");
+#define POINTGREEN(data)                printf("%s[%s*%s]%s %s%s", LETTER_LIGHTGREEN_EX, LETTER_BLUE, LETTER_LIGHTGREEN_EX, LETTER_LIGHTWHITE_EX, data, LETTER_RESET);
+#define POINTRED(data)                  printf("%s[%s*%s]%s %s%s", LETTER_LIGHTYELLOW_EX, LETTER_RED, LETTER_LIGHTYELLOW_EX, LETTER_LIGHTMAGENTA_EX, data, LETTER_RESET);
 
 #endif
