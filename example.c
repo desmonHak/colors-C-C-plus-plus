f8f94615a75a7cdf925f83ffbbc26c38e485b599
Desmon <61366533+desmonHak@users.noreply.github.com>
2022-01-23 20:19:58 +0100

    Update example.c

--- example.c
+++ example.c
@@ -1,27 +1,38 @@
 #include "colors.h"
+#include <stdio.h>
 
 int main(){
     
-    POINTGREEN("hola mundo\n", color);
-    POINTRED("hola mundo\n", color);
-    printf("%snegro\n", color.BLACK);
-    printf("%srojo\n", color.RED);
-    printf("%sverde\n", color.GREEN);
-    printf("%samarillo\n", color.YELLOW);
-    printf("%sazul\n", color.BLUE);
-    printf("%smagenta\n", color.MAGENTA);
-    printf("%scyan\n", color.CYAN);
-    printf("%sblanco\n", color.WHITE);
-    printf("%sreset color a por defecto\n", color.RESET);
+    CLEAR()
+    POINTGREEN("hola mundo\n");
+    POINTRED("hola mundo\n");
+    printf("%snegro\n", LETTER_BLACK);
+    printf("%srojo\n", LETTER_RED);
+    printf("%sverde\n", LETTER_GREEN);
+    printf("%samarillo\n", LETTER_YELLOW);
+    printf("%sazul\n", LETTER_BLUE);
+    printf("%smagenta\n", LETTER_MAGENTA);
+    printf("%scyan\n", LETTER_CYAN);
+    printf("%sblanco\n", LETTER_WHITE);
+    printf("%sreset color a por defecto\n", LETTER_RESET);
 
-    printf("%snegro claro\n", color.LIGHTBLACK_EX);
-    printf("%srojo claro\n", color.LIGHTRED_EX);
-    printf("%sverde claro\n", color.LIGHTGREEN_EX);
-    printf("%samarillo claro\n", color.LIGHTYELLOW_EX);
-    printf("%sazul claro\n", color.LIGHTBLUE_EX);
-    printf("%smagenta claro\n", color.LIGHTMAGENTA_EX);
-    printf("%scyan claro\n", color.LIGHTCYAN_EX);
-    printf("%sblanco claro\n", color.LIGHTWHITE_EX);
+    printf("%snegro claro\n", LETTER_LIGHTBLACK_EX);
+    printf("%srojo claro\n", LETTER_LIGHTRED_EX);
+    printf("%sverde claro\n", LETTER_LIGHTGREEN_EX);
+    printf("%samarillo claro\n", LETTER_LIGHTYELLOW_EX);
+    printf("%sazul claro\n", LETTER_LIGHTBLUE_EX);
+    printf("%smagenta claro\n", LETTER_LIGHTMAGENTA_EX);
+    printf("%scyan claro\n", LETTER_LIGHTCYAN_EX);
+    printf("%sblanco claro\n", LETTER_LIGHTWHITE_EX);
 
+    printf("%sfondo negro\n", BACKGROUND_BLACK);
+    printf("%sfondo rojo\n", BACKGROUND_RED);
+    printf("%sfondo verde\n", BACKGROUND_GREEN);
+    printf("%sfondo amarillo\n", BACKGROUND_YELLOW);
+    printf("%sfondo azul\n", BACKGROUND_BLUE);
+    printf("%sfondo magenta\n", BACKGROUND_PURPLE);
+    printf("%sfondo cyan\n", BACKGROUND_CYAN);
+    printf("%sfondo blanco\n", BACKGROUND_WHITE);
+    printf("%sfondo por defecto\n", BACKGROUND_RESET);
 
 }
