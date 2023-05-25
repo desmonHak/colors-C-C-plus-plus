ef6b443168e3fa32414f25b27c887899e7894f78
Desmon <aaa@gmail.com>
2023-05-25 20:47:14 +0200

    new changes

--- colors.c
+++ colors.c
@@ -5,11 +5,61 @@
 void resetColorTerminal()
 {
 #ifdef _WIN32
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+    setConsoleBackgroundColor(BACKGROUND_BLACK);
+    setConsoleForegroundColor(FOREGROUND_WHITE);
+    //setConsoleColor(FOREGROUND_MASK, 0);
+    // SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_MASK);
 #else
-    printf(LETTER_RESET);
+    printf(LETTER_RESET_ANSI);
 #endif
 }
+
+void __attribute__((destructor)) _RESET_COLOR__()
+{
+    // setConsoleColor(C_WHITE, C_BLACK);
+    // SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    resetColorTerminal();
+}
+
+#ifdef _WIN32
+void setConsoleForegroundColor(WORD foregroundColor)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    WORD attributes = consoleInfo.wAttributes;
+    attributes &=  0xFFF0; // Eliminar el color de la letra actual
+    attributes |= foregroundColor;  // Establecer el nuevo color de la letra
+    SetConsoleTextAttribute(hConsole, attributes);
+
+}
+#else
+void setConsoleForegroundColor(ConsoleColor foregroundColor)
+{
+    printf("\033[%dm", 30 + foregroundColor);
+}
+#endif
+
+#ifdef _WIN32
+void setConsoleBackgroundColor(WORD backgroundColor)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+
+    WORD attributes = consoleInfo.wAttributes;
+    attributes &= 0x000F; // Limpiar los bits de color de fondo existentes
+    attributes |= backgroundColor;
+
+    SetConsoleTextAttribute(hConsole, attributes);
+}
+#else
+    void setConsoleBackgroundColor(ConsoleColor backgroundColor)
+    {
+        printf("\033[0;%dm", 40 + backgroundColor);
+    }
+#endif
 
 #ifdef _WIN32
 void __attribute__((constructor)) _ACTIVATE_COLORS_ANSI_WIN__()
@@ -27,26 +77,23 @@ void __attribute__((constructor)) _ACTIVATE_COLORS_ANSI_WIN__()
         }
     }
 }
+#endif
 
-void __attribute__((destructor)) _RESET_COLOR__()
+#ifdef _WIN32
+void setConsoleColor(WORD foreground, WORD background)
 {
-    // setConsoleColor(C_WHITE, C_BLACK);
-    // SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-    resetColorTerminal();
-}
-
-void setConsoleColor(ConsoleColor foreground, ConsoleColor background)
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, foreground | (background << 4));
+    // HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    // SetConsoleTextAttribute(hConsole, (int)foreground | ((int)background << 4));
+    setConsoleForegroundColor(foreground);
+    setConsoleBackgroundColor(background);
 }
 #else
 void setConsoleColor(ConsoleColor foreground, ConsoleColor background)
 {
-    printf("\033[%dm\033[0;%dm", 30 + foreground, 40 + background);
+    setConsoleForegroundColor(foreground);
+    setConsoleBackgroundColor(background);
 }
 #endif
-
 void printf_color(const char *format, ...)
 {
     va_list args;
@@ -57,11 +104,12 @@ void printf_color(const char *format, ...)
 
 void vprintf_color(const char *format, va_list args)
 {
-    const char *p = format;
+    unsigned char *formatted_buffer = (unsigned char *)malloc(strlen(format) * sizeof(unsigned char));
+    vsprintf(formatted_buffer, format, args);
+    const char *p = formatted_buffer;
     bool in_color_code = false;
     char color_code[20];
     int color_code_index = 0;
-    char buffer[256] = "";
 
     while (*p != '\0')
     {
@@ -72,72 +120,108 @@ void vprintf_color(const char *format, va_list args)
                 color_code[color_code_index] = '\0';
                 if (strcmp(color_code, "FG:red") == 0)
                 {
-                    printf(LETTER_RED); // Set foreground text color to red
+                    // Cambiar a color rojo
+                    LETTER_RED;
                 }
                 else if (strcmp(color_code, "FG:reset") == 0 || strcmp(color_code, "BG:reset") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_RESET);
+                    // Restablecer colores
+                    resetColorTerminal();
                 }
                 else if (strcmp(color_code, "FG:green") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_GREEN); // Set foreground text color to green
+                    // Cambiar a color verde
+                    LETTER_GREEN;
                 }
                 else if (strcmp(color_code, "FG:blue") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_BLUE); // Set foreground text color to blue
+                    // Cambiar a color azul
+                    LETTER_BLUE;
                 }
                 else if (strcmp(color_code, "FG:black") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_BLACK); // Set foreground text color to black
+                    // Cambiar a color negro
+                    LETTER_BLACK;
                 }
                 else if (strcmp(color_code, "FG:yellow") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_YELLOW); // Set foreground text color to yellow
+                    // Cambiar a color amarillo
+                    LETTER_YELLOW;
                 }
                 else if (strcmp(color_code, "FG:purple") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_MAGENTA); // Set foreground text color to purple
+                    // Cambiar a color magenta
+                    LETTER_MAGENTA;
                 }
                 else if (strcmp(color_code, "FG:cyan") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_CYAN); // Set foreground text color to cyan
+                    // Cambiar a color cian
+                    LETTER_CYAN;
                 }
                 else if (strcmp(color_code, "FG:white") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, LETTER_WHITE); // Set foreground text color to white
+                    // Cambiar a color blanco
+                    LETTER_WHITE;
                 }
                 else if (strcmp(color_code, "BG:black") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, BACKGROUND_COLOR_BLACK); // Set background color to black
+                    // Cambiar a fondo negro
+                    BACKGROUND_COLOR_BLACK;
                 }
                 else if (strcmp(color_code, "BG:red") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, BACKGROUND_COLOR_RED); // Set background color to red
+                    // Cambiar a fondo rojo
+                    BACKGROUND_COLOR_RED;
                 }
                 else if (strcmp(color_code, "BG:green") == 0)
                 {
-                    printf(BACKGROUND_COLOR_GREEN); // Set background color to green
+                    // Cambiar a fondo verde
+                    BACKGROUND_COLOR_GREEN;
                 }
                 else if (strcmp(color_code, "BG:yellow") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, BACKGROUND_COLOR_YELLOW); // Set background color to yellow
+                    // Cambiar a fondo amarillo
+                    BACKGROUND_COLOR_YELLOW;
                 }
                 else if (strcmp(color_code, "BG:purple") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, BACKGROUND_COLOR_MAGENTA); // Set background color to purple
+                    // Cambiar a fondo magenta
+                    BACKGROUND_COLOR_MAGENTA;
                 }
                 else if (strcmp(color_code, "BG:cyan") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, BACKGROUND_COLOR_CYAN); // Set background color to cyan
+                    // Cambiar a fondo cian
+                    BACKGROUND_COLOR_CYAN;
                 }
                 else if (strcmp(color_code, "BG:white") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, BACKGROUND_COLOR_WHITE); // Set background color to white
+                    // Cambiar a fondo blanco
+                    BACKGROUND_COLOR_WHITE;
                 }
                 else if (strcmp(color_code, "BG:blue") == 0)
                 {
-                    sprintf(buffer, "%s%s", buffer, BACKGROUND_COLOR_BLUE); // Set background color to blue
+                    // Cambiar a fondo azul
+                    BACKGROUND_COLOR_BLUE;
+                } else if (strncmp(color_code, "FG:", 3) == 0)
+                {
+                    // Comprobar si es un color personalizado FG:r;g;b
+                    unsigned char red, green, blue;
+                    if (sscanf(color_code, "FG:%hhu;%hhu;%hhu", &red, &green, &blue) == 3)
+                    {
+                        // Cambiar a color personalizado
+                        foreground_color_custom(red, green, blue);
+                    }
+                }else if (strncmp(color_code, "BG:", 3) == 0)
+                {
+                    // Comprobar si es un color personalizado FG:r;g;b
+                    unsigned char red, green, blue;
+                    if (sscanf(color_code, "BG:%hhu;%hhu;%hhu", &red, &green, &blue) == 3)
+                    {
+                        // Cambiar a color personalizado
+                        background_color_custom(red, green, blue);
+                    }
                 }
+                
 
                 in_color_code = false;
                 color_code_index = 0;
@@ -160,23 +244,24 @@ void vprintf_color(const char *format, va_list args)
                 }
                 else
                 {
-                    sprintf(buffer, "%s#%c", buffer, *p);
+                    putchar('#');
+                    putchar(*p);
+                    fflush(stdout);
                 }
             }
             else
             {
-                sprintf(buffer, "%s%c", buffer, *p);
+                putchar(*p);
+                fflush(stdout);
             }
         }
 
         p++;
     }
 
-    char formatted_buffer[256];
-    vsprintf(formatted_buffer, buffer, args);
-    printf("%s" LETTER_RESET, formatted_buffer);
-    // setConsoleColor(C_WHITE, C_BLACK);
+    // Restablecer colores
     resetColorTerminal();
+
 }
 
 void clear_line()
@@ -238,8 +323,8 @@ static inline void back_fore_color_custom_RGB(RGB_C colorBackGround, RGB_C color
         colorForeGround.b);
 }
 static void back_fore_color_custom_(unsigned char redB, unsigned char greenB,
-                                       unsigned char blueB, unsigned char redF,
-                                       unsigned char greenF, unsigned char blueF)
+                                    unsigned char blueB, unsigned char redF,
+                                    unsigned char greenF, unsigned char blueF)
 {
     printf(BACK_FORE_COLOR_CUSTOM("%d", "%d", "%d", "%d", "%d", "%d"), redB, greenB, blueB, redF, greenF, blueF);
 }
--- colors.h
+++ colors.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <string.h>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -26,75 +27,180 @@ typedef enum ConsoleColor
     C_LIGHTWHITE
 } ConsoleColor;
 
+#ifndef FOREGROUND_BLACK
+#define FOREGROUND_BLACK 0
+#endif
+#ifndef FOREGROUND_WHITE
+#define FOREGROUND_WHITE FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
+//#define FOREGROUND_WHITE  0xf
+#endif
+#ifndef FOREGROUND_MASK
+#define FOREGROUND_MASK (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED)
+#endif
+#ifndef FOREGROUND_RESET
+#define FOREGROUND_RESET FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
+#endif
+#ifndef FOREGROUND_YELLOW
+#define FOREGROUND_YELLOW FOREGROUND_RED | FOREGROUND_GREEN 
+#endif
+#ifndef FOREGROUND_MAGENTA
+#define FOREGROUND_MAGENTA FOREGROUND_RED | FOREGROUND_BLUE 
+#endif
+#ifndef FOREGROUND_CYAN
+#define FOREGROUND_CYAN FOREGROUND_GREEN | FOREGROUND_BLUE 
+#endif
+
+#ifndef BACKGROUND_BLACK
+#define BACKGROUND_BLACK 0
+#endif
+#ifndef BACKGROUND_WHITE
+#define BACKGROUND_WHITE 0xf0
+#endif
+#ifndef BACKGROUND_YELLOW
+#define BACKGROUND_YELLOW BACKGROUND_RED | BACKGROUND_GREEN
+#endif
+#ifndef BACKGROUND_MAGENTA
+#define BACKGROUND_MAGENTA BACKGROUND_RED | BACKGROUND_BLUE
+#endif
+#ifndef BACKGROUND_CYAN
+#define BACKGROUND_CYAN BACKGROUND_GREEN | BACKGROUND_BLUE
+#endif
+
 void __attribute__((constructor)) _ACTIVATE_COLORS_ANSI_WIN__();
 #else
 typedef enum ConsoleColor
 {
     C_BLACK = 0,
-    C_BLUE = 4,
-    C_GREEN = 2,
-    C_CYAN = 6,
     C_RED = 1,
-    C_MAGENTA = 5,
+    C_GREEN = 2,
     C_YELLOW = 3,
+    C_BLUE = 4,
+    C_MAGENTA = 5,    
+    C_CYAN = 6,
     C_WHITE = 7,
+    
     C_LIGHTBLACK = 0,
-    C_LIGHTCYAN = 6,
     C_LIGHTRED = 1,
-    C_LIGHTMAGENTA = 5,
+    C_LIGHTGREEN = 2,
     C_LIGHTYELLOW = 3,
+    C_LIGHTBLUE = 4,
+    C_LIGHTMAGENTA = 5,
+    C_LIGHTCYAN = 6,
     C_LIGHTWHITE = 7
 } ConsoleColor;
 #endif
 
-#define BACKGROUND_COLOR_BLACK "\033[0;40m"
-#define BACKGROUND_COLOR_RED "\033[0;41m"
-#define BACKGROUND_COLOR_GREEN "\033[0;42m"
-#define BACKGROUND_COLOR_YELLOW "\033[0;43m"
-#define BACKGROUND_COLOR_BLUE "\033[0;44m"
-#define BACKGROUND_COLOR_MAGENTA "\033[0;45m"
-#define BACKGROUND_COLOR_CYAN "\033[0;46m"
-#define BACKGROUND_COLOR_WHITE "\033[0;47m"
-#define BACKGROUND_COLOR_RESET "\033[0m"
+#define BACKGROUND_COLOR_BLACK_ANSI   "\033[0;40m"
+#define BACKGROUND_COLOR_RED_ANSI     "\033[0;41m"
+#define BACKGROUND_COLOR_GREEN_ANSI   "\033[0;42m"
+#define BACKGROUND_COLOR_YELLOW_ANSI  "\033[0;43m"
+#define BACKGROUND_COLOR_BLUE_ANSI    "\033[0;44m"
+#define BACKGROUND_COLOR_MAGENTA_ANSI "\033[0;45m"
+
+#define BACKGROUND_COLOR_CYAN_ANSI    "\033[0;46m"
+#define BACKGROUND_COLOR_WHITE_ANSI   "\033[0;47m"
+#define BACKGROUND_COLOR_RESET_ANSI   "\033[0m"
+
+#ifdef _WIN32
+#define BACKGROUND_COLOR_GREEN       setConsoleBackgroundColor(BACKGROUND_GREEN)
+#define BACKGROUND_COLOR_YELLOW      setConsoleBackgroundColor(BACKGROUND_YELLOW)
+#define BACKGROUND_COLOR_BLUE        setConsoleBackgroundColor(BACKGROUND_BLUE)
+#define BACKGROUND_COLOR_RED         setConsoleBackgroundColor(BACKGROUND_RED)
+#define BACKGROUND_COLOR_BLACK       setConsoleBackgroundColor(BACKGROUND_BLACK)
+#define BACKGROUND_COLOR_MAGENTA     setConsoleBackgroundColor(BACKGROUND_MAGENTA)
+#define BACKGROUND_COLOR_CYAN        setConsoleBackgroundColor(BACKGROUND_CYAN)
+#define BACKGROUND_COLOR_WHITE       setConsoleBackgroundColor(BACKGROUND_WHITE)
+#else
+#define BACKGROUND_COLOR_GREEN       setConsoleBackgroundColor(C_GREEN)
+#define BACKGROUND_COLOR_YELLOW      setConsoleBackgroundColor(C_YELLOW)
+#define BACKGROUND_COLOR_BLUE        setConsoleBackgroundColor(C_BLUE)
+#define BACKGROUND_COLOR_RED         setConsoleBackgroundColor(C_RED)
+#define BACKGROUND_COLOR_BLACK       setConsoleBackgroundColor(C_BLACK)
+#define BACKGROUND_COLOR_MAGENTA     setConsoleBackgroundColor(C_MAGENTA)
+#define BACKGROUND_COLOR_CYAN        setConsoleBackgroundColor(C_CYAN)
+#define BACKGROUND_COLOR_WHITE       setConsoleBackgroundColor(C_WHITE)
+#endif 
+#define BACKGROUND_COLOR_RESET       resetColorTerminal()
+
+
+
 
 // letra negro oscuro
-#define LETTER_BLACK "\033[30m"
+#define LETTER_BLACK_ANSI   "\033[30m"
 
 // letra rojo ocuro
-#define LETTER_RED "\033[31m"
+#define LETTER_RED_ANSI     "\033[31m"
 
 // letra verde ocuro
-#define LETTER_GREEN "\033[32m"
+#define LETTER_GREEN_ANSI   "\033[32m"
 
 // letra amarillo oscuro
-#define LETTER_YELLOW "\033[33m"
+#define LETTER_YELLOW_ANSI  "\033[33m"
 
 // letra azul oscuro
-#define LETTER_BLUE "\033[34m"
+#define LETTER_BLUE_ANSI    "\033[34m"
 
 // letra magenta oscuro
-#define LETTER_MAGENTA "\033[35m"
+#define LETTER_MAGENTA_ANSI "\033[35m"
 
 // letra cyan ocuro:
-#define LETTER_CYAN "\033[36m"
+#define LETTER_CYAN_ANSI    "\033[36m"
 
 // letra blanco oscuro(gris claro)
-#define LETTER_WHITE "\033[37m"
+#define LETTER_WHITE_ANSI   "\033[37m"
 
 // resetear el color de la letra a la por defecto
-#define LETTER_RESET "\033[38m"
+#define LETTER_RESET_ANSI   "\033[38m"
 
 // versiones claras de los colores de letra:
 
 // fondo negro:
-#define LETTER_LIGHTBLACK_EX "\033[90m" // o gris
-#define LETTER_LIGHTRED_EX "\033[91m"
-#define LETTER_LIGHTGREEN_EX "\033[92m"
-#define LETTER_LIGHTYELLOW_EX "\033[93m"
-#define LETTER_LIGHTBLUE_EX "\033[94m"
-#define LETTER_LIGHTMAGENTA_EX "\033[95m"
-#define LETTER_LIGHTCYAN_EX "\033[96m"
-#define LETTER_LIGHTWHITE_EX "\033[97m"
+#define LETTER_LIGHTBLACK_EX_ANSI "\033[90m" // o gris
+#define LETTER_LIGHTRED_EX_ANSI "\033[91m"
+#define LETTER_LIGHTGREEN_EX_ANSI "\033[92m"
+#define LETTER_LIGHTYELLOW_EX_ANSI "\033[93m"
+#define LETTER_LIGHTBLUE_EX_ANSI "\033[94m"
+#define LETTER_LIGHTMAGENTA_EX_ANSI "\033[95m"
+#define LETTER_LIGHTCYAN_EX_ANSI "\033[96m"
+#define LETTER_LIGHTWHITE_EX_ANSI "\033[97m"
+
+
+#ifdef _WIN32
+#define LETTER_GREEN            setConsoleForegroundColor(FOREGROUND_GREEN)
+#define LETTER_YELLOW           setConsoleForegroundColor(FOREGROUND_YELLOW)
+#define LETTER_BLUE             setConsoleForegroundColor(FOREGROUND_BLUE)
+#define LETTER_RED              setConsoleForegroundColor(FOREGROUND_RED)
+#define LETTER_BLACK            setConsoleForegroundColor(FOREGROUND_BLACK)
+#define LETTER_MAGENTA          setConsoleForegroundColor(FOREGROUND_MAGENTA)
+#define LETTER_CYAN             setConsoleForegroundColor(FOREGROUND_CYAN)
+#define LETTER_WHITE            setConsoleForegroundColor(FOREGROUND_WHITE)
+#define LETTER_LIGHTBLACK_EX    setConsoleForegroundColor(FOREGROUND_BLACK   | FOREGROUND_INTENSITY)
+#define LETTER_LIGHTRED_EX      setConsoleForegroundColor(FOREGROUND_RED     | FOREGROUND_INTENSITY)
+#define LETTER_LIGHTGREEN_EX    setConsoleForegroundColor(FOREGROUND_GREEN   | FOREGROUND_INTENSITY)
+#define LETTER_LIGHTYELLOW_EX   setConsoleForegroundColor(FOREGROUND_YELLOW  | FOREGROUND_INTENSITY)
+#define LETTER_LIGHTBLUE_EX     setConsoleForegroundColor(FOREGROUND_BLUE    | FOREGROUND_INTENSITY)
+#define LETTER_LIGHTMAGENTA_EX  setConsoleForegroundColor(FOREGROUND_MAGENTA | FOREGROUND_INTENSITY)
+#define LETTER_LIGHTCYAN_EX     setConsoleForegroundColor(FOREGROUND_CYAN    | FOREGROUND_INTENSITY)
+#define LETTER_LIGHTWHITE_EX    setConsoleForegroundColor(FOREGROUND_WHITE   | FOREGROUND_INTENSITY)
+#else
+#define LETTER_GREEN             setConsoleForegroundColor(C_GREEN)
+#define LETTER_YELLOW            setConsoleForegroundColor(C_YELLOW)
+#define LETTER_BLUE              setConsoleForegroundColor(C_BLUE)
+#define LETTER_RED               setConsoleForegroundColor(C_RED)
+#define LETTER_BLACK             setConsoleForegroundColor(C_BLACK)
+#define LETTER_MAGENTA           setConsoleForegroundColor(C_MAGENTA)
+#define LETTER_CYAN              setConsoleForegroundColor(C_CYAN)
+#define LETTER_WHITE             setConsoleForegroundColor(C_WHITE)
+#define LETTER_LIGHTBLACK_EX     setConsoleForegroundColor(C_LIGHTBLACK)
+#define LETTER_LIGHTRED_EX       setConsoleForegroundColor(C_LIGHTRED)
+#define LETTER_LIGHTGREEN_EX     setConsoleForegroundColor(C_LIGHTGREEN)
+#define LETTER_LIGHTYELLOW_EX    setConsoleForegroundColor(C_LIGHTYELLOW)
+#define LETTER_LIGHTBLUE_EX      setConsoleForegroundColor(C_LIGHTBLUE)
+#define LETTER_LIGHTMAGENTA_EX   setConsoleForegroundColor(C_LIGHTMAGENTA)
+#define LETTER_LIGHTCYAN_EX      setConsoleForegroundColor(C_LIGHTCYAN)
+#define LETTER_LIGHTWHITE_EX     setConsoleForegroundColor(C_LIGHTWHITE)
+#endif 
+#define LETTER_RESET       resetColorTerminal()
 
 // stylos:
 #define STYLE_BOLDED "\033[1m"     // : Negrita
@@ -182,7 +288,15 @@ void __attribute__((destructor)) _RESET_COLOR__();
 void clear_line();
 void clear_display();
 void set_title(char *title);
+#ifdef _WIN32
+void setConsoleForegroundColor(WORD foregroundColor);
+void setConsoleBackgroundColor(WORD backgroundColor);
+void setConsoleColor(WORD foreground, WORD background);
+#else
+void setConsoleForegroundColor(ConsoleColor foregroundColor);
+void setConsoleBackgroundColor(ConsoleColor backgroundColor);
 void setConsoleColor(ConsoleColor foreground, ConsoleColor background);
+#endif
 void resetColorTerminal();
 void pos(unsigned char x, unsigned char y, char *data);
 void back(char *data, unsigned char number);
