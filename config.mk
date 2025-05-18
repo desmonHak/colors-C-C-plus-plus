c39d3fd59b3b5f5459f97898f09d6b8ecd14a3fa
desmonHak <61366533+desmonHak@users.noreply.github.com>
2025-05-18 22:17:00 +0200

    add macros for linux

--- config.mk
+++ config.mk
@@ -13,7 +13,7 @@ GLOBAL_CFLAGS = -std=c$(VESRION_C) $(INCLUDE_FLAGS) -masm=intel \
 
 ifeq ($(OS_NAME),windows)
 else
-	GLOBAL_CFLAGS += -fPIC
+	GLOBAL_CFLAGS += -fPIC -D_GNU_SOURCE  -D_POSIX_C_SOURCE=200809L
 endif
 
 CFLAGS 		  =  $(GLOBAL_CFLAGS) -O3 -Wno-unused-parameter \
